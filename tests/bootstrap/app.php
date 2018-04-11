51173a727c016fcfc3d528577bf747324a133ba9
Robbie Averill <robbie@silverstripe.com>
2018-04-11 19:05:13 +1200

    FIX Fixture the 'app' dir instead of mysite in unit tests

--- tests/bootstrap/app.php
+++ tests/bootstrap/app.php
@@ -4,6 +4,6 @@ if (!defined('BASE_PATH')) {
     define('BASE_PATH', getcwd());
 }
 
-require_once BASE_PATH . '/cms/tests/bootstrap/mysite.php';
+require_once BASE_PATH . '/cms/tests/bootstrap/app.php';
 
 copy(__DIR__ . '/fixtures/theme.yml.fixture', $projectPath . '/_config/dummytheme.yml');
